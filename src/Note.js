d7ce2f0b200b3dc9469ec91d993b137786a56415
Naomi Urnes <naomiurnes@gmail.com>
2017-06-14 14:47:59 -0400

    Form 'works.' Sends data to paragraphs, not NoteList component

---
+++ src/Note.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Note extends Component {
+    render (){
+        return (
+            <p>
+                {this.props.text}
+            </p>
+        )
+    }
+}
+
+export default Note
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,21 +1,42 @@
 import React, {Component} from 'react'
 import './General.css'
 import './NoteForm.css'
+import Note from './Note'
 
 
 class NoteForm extends Component {
+    constructor() {
+      super()
+      this.state = {
+        notes: []
+      }
+    }
+
+    handleSubmit(ev){
+      ev.preventDefault()
+      const notes = [...this.state.notes]
+      notes.push(`${this.titleField.value} | ${this.noteField.value}`)
+      this.setState({ notes })
+      ev.currentTarget.reset()
+    }
+
     render (){
         return(
-             <div className="NoteForm">
-        <form>
-          <p>
-            <input type="text" name="title" placeholder="Title your note" value="" />
-          </p>
-          <p>
-            <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
-          </p>
-        </form>
-      </div>
+            <div className="NoteForm">
+              <form onSubmit={this.handleSubmit.bind(this)}>
+              <p>
+                <input type="text" name="title" placeholder="Title your note" ref={(input) => this.titleField = input} />
+              </p>
+              <p>
+                <textarea name="body" cols="30" rows="10" placeholder="Just start typing..." ref={(input) => this.noteField = input}></textarea>
+              </p>
+              <p>
+                <input type="submit" name="send" value="Submit" />
+              </p>
+            </form>
+
+            {this.state.notes.reverse().map(curr => <Note text={curr}/>)}
+          </div>
         )
     }
 }
