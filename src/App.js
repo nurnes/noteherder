456c353efe64fe4df547140ee41da7a525d7a407
Naomi Urnes <naomiurnes@gmail.com>
2017-06-15 09:54:31 -0400

    Move notes array to App's state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ import Main from './Main'
 
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+            notes: [{title: "Citizens of distant epochs", body: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?"},
+          {title: "Preserve and cherish that pale blue dot ", body: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!"},
+          {title: "Laws of physics", body: "Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas"}]
+        }
+  }
   render() {
     return (
       <div className="App">
-        <Main />
+        <Main notes={this.state.notes}/>
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -8,12 +8,10 @@ import './General.css'
 import './Main.css'
 
 class Main extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
-            notes: [{title: "Citizens of distant epochs", body: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?"},
-          {title: "Preserve and cherish that pale blue dot ", body: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!"},
-          {title: "Laws of physics", body: "Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas"}]
+            notes: this.props.notes
         }
     }
     fetch(note){
