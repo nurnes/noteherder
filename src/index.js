e00cfa39a6ce3f20d59966defe4d0e7509fdde55
Naomi Urnes <nurnes@purdue.edu>
2017-06-21 09:59:28 -0400

    Add router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route component={App} />
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
