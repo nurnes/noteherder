7a764feb2f56d4a8256104b105aea04d267d28b3
Naomi Urnes <nurnes@purdue.edu>
2017-06-21 11:45:56 -0400

    Click note, change url

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
   }
 
   componentWillMount() {
+    this.getUserFromLocalStorage()
     auth.onAuthStateChanged(
       (user) => {
         if (user) {
@@ -25,6 +26,12 @@ class App extends Component {
         }
       }
     )
+  }
+
+  getUserFromLocalStorage() {
+    const uid = localStorage.getItem('uid')
+    if(!uid) return
+    this.setState({ uid })
   }
 
   syncNotes = () => {
@@ -72,6 +79,7 @@ class App extends Component {
   }
 
   authHandler = (userData) => {
+    localStorage.setItem('uid', userData.uid)
     this.setState(
       { uid: userData.uid },
       this.syncNotes
@@ -84,6 +92,7 @@ class App extends Component {
       .then(() => {
         base.removeBinding(this.ref)
         this.resetCurrentNote()
+        localStorage.removeItem('uid')
         this.setState({ uid: null, notes: {} }
         )})
   }
